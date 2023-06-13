a4d05e04e4251e6b99ad54a3067781fa7884ed7c
antoine <110190591+antoineskt@users.noreply.github.com>
2023-06-13 23:40:56 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App(props) {
       <form>
         <h2 className="label-wrapper">
           <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
+            aDQSFQD
           </label>
         </h2>
         <input
