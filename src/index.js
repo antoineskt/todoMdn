05da65df327ee54d679cde942c0760961f02e483
antoine <110190591+antoineskt@users.noreply.github.com>
2023-06-15 00:30:14 +0000

    Update 7 files

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
+
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+];
+
+
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <App tasks={DATA}/>
   </StrictMode>
 );
